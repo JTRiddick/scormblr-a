17ef31273f34693a001ceedb70be40b2b9e230bd
Tatrakrad <riddick.taylor@gmail.com>
2017-06-15 22:37:53 -0500

    fixed packages, added a layout component

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,19 @@ import promise from 'redux-promise';
 import reducers from '../reducers';
 import Index from './index';
 import About from './about';
+import Layout from './Layout';
+import NotFound from './NotFound';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 export const App = () => (
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Switch>
-      <Route path="/about" component={About} />
-      <Route path="/" component={Index} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Index} />
+      </Switch>
+    </Layout>
   </Provider>)
 
 export default App;
