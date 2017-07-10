ed04feaa50b7f494ae02d9788f527b6235157e69
Tatrakrad <riddick.taylor@gmail.com>
2017-07-10 12:54:08 -0500

    remover working, scrambler broken

--- src/reducers/card_reducer.js
+++ src/reducers/card_reducer.js
@@ -5,7 +5,9 @@ const initialState = {
   iterations:0
 }
 const cardScramble = (cards,n) => {
-  let iterations = n
+  let iterations = n;
+  // is returning a new object necessary for immutable state change in reducer?
+  let newCards = {};
 
   cards.forEach((card,i)=>{
     let rngI = Math.floor(Math.random() * cards.length);
@@ -14,7 +16,10 @@ const cardScramble = (cards,n) => {
     cards[rngI] = temp;
   })
   if(n < 1){
-    return cards;
+    newCards = cards;
+    // console.log('shuffled cards to :',newCards); //its an array of objects
+    // dumbass
+    return newCards;
   }else{
     iterations--;
     cardScramble(cards,iterations);
@@ -30,10 +35,13 @@ export default function cardReducer(state = initialState,action){
       // return Object.assign({},state,state.cards.push({name: action.name, text:action.text}));
       return Object.assign({},state,{cards:[...state.cards,{name:action.name,text:action.text}]});
     case REMOVE_CARD:
-      return Object.assign({},state,state.cards.pop());
+      return Object.assign( {},state,{cards:state.cards.slice(0,-1)} );
     case SCRAMBLE:
       if(action.iterations > 1 && state.cards.length > 1){
-       return Object.assign({},state,{cards:[...cardScramble(state.cards,action.iterations)]});
+        let newCards = state.cards;
+        newCards = cardScramble(newCards,action.iterations);
+        console.log('newcards :', newCards); // returns undefined but works on remount
+       return Object.assign({},state,{cards:[...newCards]});
      }else {
        return state;
      }
